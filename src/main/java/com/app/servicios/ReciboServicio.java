aa7b27fd988e54c6eee932078e8fcccec93c8b78
WilmarTarazona <alonso_tch@hotmail.com>
2021-11-16 15:49:22 -0500

    Actualizar ReciboServicio

--- src/main/java/com/app/servicios/ReciboServicio.java
+++ src/main/java/com/app/servicios/ReciboServicio.java
@@ -3,7 +3,10 @@ package com.app.servicios;
 import com.app.dto.ReciboDto;
 import com.app.excepciones.AppException;
 
+import java.util.List;
+
 public interface ReciboServicio {
     ReciboDto crearRecibo(ReciboDto reciboDto) throws AppException;
     ReciboDto detallesRecibo(Long id) throws AppException;
+    List<ReciboDto> mostrarRecibos(Long carteraID) throws AppException;
 }
