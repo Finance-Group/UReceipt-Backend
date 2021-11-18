465138611a1447ea8831097ba34507056afb2661
Julissa Ponte <66703701+JPonte09@users.noreply.github.com>
2021-11-18 17:34:05 -0500

    fix: rename 2 repositories (interfaces)

--- src/main/java/com/app/repositorios/CarteraGastosFinalesRepository.java
+++ src/main/java/com/app/repositorios/CarteraGastosFinalesRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface CarteraGastoFinalRepository extends JpaRepository<CarteraGastoFinal,Long> {
-    // TODO: find by CCartera
+public interface CarteraGastosFinalesRepository extends JpaRepository<CarteraGastoFinal,Long> {
     List<CarteraGastoFinal> getAllByCarteraId (Long carteraId);
     Optional<CarteraGastoFinal> findByCarteraIdAndGastofinalId (Long carteraId, Long gastoFinalId);
 }
--- src/main/java/com/app/repositorios/CarteraGastosInicialesRepository.java
+++ src/main/java/com/app/repositorios/CarteraGastosInicialesRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface CarteraGastoInicialRepository extends JpaRepository<CarteraGastoInicial,Long> {
-    // TODO: find by CCartera
+public interface CarteraGastosInicialesRepository extends JpaRepository<CarteraGastoInicial,Long> {
     List<CarteraGastoInicial> getAllByCarteraId (Long carteraId);
     Optional<CarteraGastoInicial> findByCarteraIdAndGastoinicialId (Long carteraId, Long gastoInicialId);
 }
